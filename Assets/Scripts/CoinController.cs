135e6b0d42878cd88e9cd7d9f46b8e6e327784db
Krosulhah <giulia.valcamonica@mail.polimi.it>
2022-10-25 16:15:08 +0200

    Start to work on coin collection (still in discussion)

---
+++ Assets/Scripts/CoinController.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinController : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        //check if a bouncey element is touched by the stickman
+        if (col.gameObject.CompareTag("Stickman")){
+            EventManager.TriggerEvent("OnCoin"); 
+        }
+    }
+   
+}
--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -7,16 +7,33 @@ namespace ParkourNews.Scripts
     {
         //to reset stickman initial position upon death
         private Vector2 _initialPosition;
+        private double _playerPoints;
+        [SerializeField] private double _coinValue = 1;
         
+        private void Awake()
+        {
+            _playerPoints = 0;
+        }
+
         public void OnEnable()
         {
            EventManager.StartListening("OnPlayerDeath",OnPlayerDeath);
+           EventManager.StartListening("OnCoin",OnCoin);
            _initialPosition = FindObjectOfType<StickmanController>().transform.position;
         }
 
         private void OnPlayerDeath()
         {
             FindObjectOfType<StickmanController>().transform.position = _initialPosition;
+        }
+
+        private void OnCoin()
+        {
+            EventManager.StopListening("OnCoin",OnCoin);
+            _playerPoints+= _coinValue;
+            Debug.Log("Collected Coin!");
+            EventManager.StartListening("OnCoin",OnCoin);
+            //todo delete coin
         }
     }
 }
