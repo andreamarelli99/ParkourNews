f5825cda719cfb4f9796b3b5464378dd53fd7286
Krosulhah <giulia.valcamonica@mail.polimi.it>
2022-12-20 19:42:13 +0100

    fix

--- Assets/Scripts/MainMenu/MenuController.cs
+++ Assets/Scripts/MainMenu/MenuController.cs
@@ -24,15 +24,26 @@ public class MenuController : MonoBehaviour
     private Button _backButton;
     private LevelManager _levelManager;
 
+    [SerializeField] private GameObject keyboardSettings;
+    [SerializeField] private GameObject XBOXSettings;
+    [SerializeField] private GameObject PS4Settings;
+    [SerializeField] private Button rightSettingsButton;
+    [SerializeField] private Button leftSettingsButton;
+
     [SerializeField] private GameObject firstSelectedButtonMainMenu;
     [SerializeField] private GameObject firstSelectedButtonOptions;
     [SerializeField] private GameObject firstSelectedButtonExitOptions;
+    
 
     private int _numberOfLevels;
     
     public void Start()
     
     {
+        rightSettingsButton.onClick.AddListener(RightButton);
+        leftSettingsButton.onClick.AddListener(LeftButton);
+        
+        keyboardSettings.SetActive(true);
         _levelManager = GameObject.FindObjectOfType<LevelManager>();
         _numberOfLevels = _levelManager.numberOfLevels();
         
@@ -104,6 +115,44 @@ public class MenuController : MonoBehaviour
         EventSystem.current.SetSelectedGameObject(null);
         //set play as the selected object
         EventSystem.current.SetSelectedGameObject(firstSelectedButtonExitOptions);
+    }
+
+    public void RightButton()
+    {
+        if (keyboardSettings.activeSelf)
+        {
+            keyboardSettings.SetActive(false);
+            XBOXSettings.SetActive(true);
+        }
+        else if (XBOXSettings.activeSelf)
+        {
+            XBOXSettings.SetActive(false);
+            PS4Settings.SetActive(true);
+        }
+        else
+        {
+            PS4Settings.SetActive(false);
+            keyboardSettings.SetActive(true);
+        }
+    }
+
+    public void LeftButton()
+    {
+        if (PS4Settings.activeSelf)
+        {
+            PS4Settings.SetActive(false);
+            XBOXSettings.SetActive(true);
+        }
+        else if (keyboardSettings.activeSelf)
+        {
+            keyboardSettings.SetActive(false);
+            PS4Settings.SetActive(true);
+        }
+        else
+        {
+            XBOXSettings.SetActive(false);
+            keyboardSettings.SetActive(true);
+        }
     }
     
 }
